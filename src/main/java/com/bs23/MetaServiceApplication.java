17a58fafe3c61b87023e1b17bea692f15a236a0c
khairulbs23 <khairul.islam@brainstation-23.com>
2023-03-19 01:17:20 +0600

    meta service

---
+++ src/main/java/com/bs23/MetaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bs23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaServiceApplication.class, args);
+    }
+
+}
